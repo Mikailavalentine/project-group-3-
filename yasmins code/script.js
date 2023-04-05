bb2d96ae87ba68fbfdcdc33e746d80789d48be64
Ryan Lierman <rylierman@gmail.com>
2023-04-05 13:25:15 -0700

    Added an ugly but working version of the API into the exsisting front-end code, pulled values display under the premade recipes that are stagnant

--- yasmins code/script.js
+++ yasmins code/script.js
@@ -55,3 +55,37 @@ registerForm.addEventListener("submit", async (event) => {
     alert("An error occurred, please try again later.");
   }
 });
+
+//API function
+
+        // This is the area for the API key
+        const apiKey = "0043711dcf8a417e89708a1af0cfbaa7";
+    
+        function getsource(id){
+            $.ajax({
+                url:`https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`,
+                success:function(res){
+                    document.getElementById("sourceLink").innerHTML=res.sourceUrl;
+                    document.getElementById("sourceLink").href=res.sourceUrl;
+                },
+                error:function(err){
+                    console.log(err);
+                }
+            });
+        }
+
+        function getrecipe(q){
+            $.ajax({
+                url:`https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&number=1&query=${q}`,
+                success:function(res){
+                    const output = document.getElementById("output");
+                    
+    /*The below line changes the displayed elements and creates the box which stores the pulled API recipes */                
+    output.innerHTML = `<h1>${res.results[0].title}</h1><br><img src="${res.baseUri}${res.results[0].image}" width="400"/> <br> ready in ${res.results[0].readyInMinutes} minutes`;
+                    getsource(res.results[0].id);
+                },
+                error:function(err){
+                    console.log(err);
+                }
+            });
+        }
