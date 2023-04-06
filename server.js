669c328c2568f28e369249e0afab2a50c3ee768f
Ryan Lierman <rylierman@gmail.com>
2023-04-05 21:10:34 -0700

    this is temp work on it later

--- server.js
+++ server.js
@@ -1,48 +1,16 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// not sure if this is correct if someone could check that would be nice %^&*
-
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
-const sequelize = require('./config/connection');
-=======
 const path = require('path');
-=======
-
-require('dotenv').config();
 const exphbs = require('express-handlebars');
->>>>>>> 2b40e7ce669ad51430f59a4bc602fcc0f706338f
-const express = require('express');
-const path = require('path');
-const sequelize = require('./config/connection');
-<<<<<<< HEAD
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
->>>>>>> d017c47cd5bae3b8020fdd0e0c7f2936ccdc719b
-=======
 const router = require('./controllers');
-const sessionMiddleware = require('./config/session');
->>>>>>> 2b40e7ce669ad51430f59a4bc602fcc0f706338f
+const sequelize = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-});
-=======
-// Set up Handlebars.js engine with custom helpers
-const hbs = express.create({  });
-
-const express = {
+// set up session
+const sessionMiddleware = session({
   secret: 'Super secret secret',
   cookie: {
     maxAge: 300000,
@@ -55,45 +23,24 @@ const express = {
   store: new SequelizeStore({
     db: sequelize
   })
-};
+});
 
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-=======
-// setup app middleware
+// set up middleware and engine
 app.use(sessionMiddleware);
-app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', exphbs({}));
->>>>>>> 2b40e7ce669ad51430f59a4bc602fcc0f706338f
-app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 // connect routes
 app.use(router);
 
-<<<<<<< HEAD
- sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
->>>>>>> d017c47cd5bae3b8020fdd0e0c7f2936ccdc719b
-=======
-// connect db and listen
-sequelize
-  .sync({ force: false })
+// connect to db and listen
+sequelize.sync({ force: false })
   .then(() => {
-    app.listen(PORT, (err) => {
-      if (err) {
-        console.error(err);
-        return process.exit(1);
-      }
-      console.log(`App listening on PORT ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
   })
   .catch((err) => {
     console.error(err);
-    process.exit(1);
   });
->>>>>>> 2b40e7ce669ad51430f59a4bc602fcc0f706338f
