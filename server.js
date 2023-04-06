2b40e7ce669ad51430f59a4bc602fcc0f706338f
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2023-04-05 19:57:19 -0700

    fixed everything

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
-const path = require('path');
+
+require('dotenv').config();
+const exphbs = require('express-handlebars');
 const express = require('express');
-const session = require('express-session');
-const express = require('express-handlebars');
-const routes = require('./controllers');
-//const helpers = require('./utils/helpers');
-
+const path = require('path');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const router = require('./controllers');
+const sessionMiddleware = require('./config/session');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = express.create({  });
-
-const express = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
+// setup app middleware
+app.use(sessionMiddleware);
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs({}));
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// connect routes
+app.use(router);
 
- sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// connect db and listen
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.error(err);
+        return process.exit(1);
+      }
+      console.log(`App listening on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
